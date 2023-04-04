0cb88e9e6cc23a96410230e362ca921d131f39e8
Andrea Jacobs <jacobsandrea02@gmail.com>
2023-04-04 19:33:25 +0200

    This commit adds the Tier enum which sets the various tiers a customer can reach.

---
+++ src/main/java/za/ac/cput/domain/loyaltypoints/Tier.java
@@ -0,0 +1,10 @@
+/** Tier.java
+ *  This program is an enum because it would just set the tier the customer is on
+ *  Andrea Jacobs 218024266
+ *  04 April 2023 */
+
+package za.ac.cput.domain.loyaltypoints;
+
+public enum Tier {
+    PLATINUM, SILVER, GOLD, BRONZE
+}
