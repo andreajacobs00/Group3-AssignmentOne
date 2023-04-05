f8cac6f3571405940d6abcda96365a1358deed4f
Haydn-Abrahams <abrahamshaydn@gmail.com>
2023-04-05 22:07:17 +0200

    This commit adds the CustomerAddress POJO - Haydn Abrahams

--- src/main/java/za/ac/cput/domain/CustomerAddress.java
+++ src/main/java/za/ac/cput/domain/CustomerAddress.java
@@ -1,29 +1,36 @@
-/*
- * Address POJO Class
- * Author: Haydn Abrahams
- * Date: 04 April 2023
- */
 package za.ac.cput.domain;
 
+import java.util.Date;
+
 public class CustomerAddress {
-    private String houseNumber;
+    private String address_ID;
+    //private Customer cust_ID;
+    private String house_No;
     private String streetName;
     private String suburb;
-    private String postalCode;
-    private Customer customerId;
+    private long postal_Code;
 
-    public CustomerAddress(Builder builder) {
-        this.houseNumber = builder.houseNumber;
+    public CustomerAddress (Builder builder) {
+        this.address_ID = builder.address_ID;
+        //this.cust_ID = builder.cust_ID;
+        this.house_No = builder.house_No;
         this.streetName = builder.streetName;
         this.suburb = builder.suburb;
-        this.postalCode = builder.postalCode;
-        this.customerId = builder.customerId;
+        this.postal_Code = Long.parseLong(builder.postal_code);
+    }
+    public CustomerAddress() {
     }
 
-    public CustomerAddress() {}
+    public String getAddress_ID() {
+        return address_ID;
+    }
 
-    public String getHouseNumber() {
-        return houseNumber;
+    /*public Customer getCust_ID() {
+        return cust_ID;
+    } */
+
+    public String getHouse_No() {
+        return house_No;
     }
 
     public String getStreetName() {
@@ -34,33 +41,36 @@ public class CustomerAddress {
         return suburb;
     }
 
-    public String getPostalCode() {
-        return postalCode;
-    }
-
-    public Customer customerID() {
-        return customerId;
+    public long getPostal_Code() {
+        return postal_Code;
     }
 
     @Override
     public String toString() {
         return "CustomerAddress{" +
-                "houseNumber='" + houseNumber + '\'' +
+                "address_ID='" + address_ID + '\'' +
+                //", cust_ID=" + cust_ID +
+                ", house_No='" + house_No + '\'' +
                 ", streetName='" + streetName + '\'' +
                 ", suburb='" + suburb + '\'' +
-                ", postalCode='" + postalCode + '\'' +
-                ", customerID='" + customerId + '\'' +
+                ", postal_Code=" + postal_Code +
                 '}';
     }
-
     public static class Builder {
-        private String houseNumber;
+        private String address_ID;
+        //private Customer cust_ID;
+        private String house_No;
         private String streetName;
         private String suburb;
-        private String postalCode;
-        private Customer customerId;
-        public Builder setHouseNumber(String houseNumber) {
-            this.houseNumber = houseNumber;
+        private String postal_code;
+
+        public Builder setAddress_ID(String address_ID) {
+            this.address_ID = address_ID;
+            return this;
+        }
+
+        public Builder setHouse_No(String house_No) {
+            this.house_No = house_No;
             return this;
         }
 
@@ -74,17 +84,18 @@ public class CustomerAddress {
             return this;
         }
 
-        public Builder setPostalCode(String postalCode) {
-            this.postalCode = postalCode;
+        public Builder setPostal_code(String postal_code) {
+            this.postal_code = postal_code;
             return this;
         }
 
-        public Builder copy(CustomerAddress address) {
-            this.houseNumber = address.houseNumber;
-            this.streetName = address.streetName;
-            this.suburb = address.suburb;
-            this.postalCode = address.postalCode;
-            this.customerId = address.customerId;
+        public Builder copy(CustomerAddress customerAddress) {
+            this.address_ID = customerAddress.address_ID;
+            //this.cust_ID = customerAddress.cust_ID;
+            this.house_No = customerAddress.house_No;
+            this.streetName = customerAddress.streetName;
+            this.suburb = customerAddress.suburb;
+            this.postal_code = String.valueOf(customerAddress.postal_Code);
             return this;
         }
 
