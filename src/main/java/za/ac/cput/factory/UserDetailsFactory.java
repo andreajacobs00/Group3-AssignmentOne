572a8d1de4ef60351414a232cafd1c0b308629ca
Haydn-Abrahams <abrahamshaydn@gmail.com>
2023-04-07 09:21:12 +0200

    This Commit adds the UserDetailsFactory - Haydn Abrahams

--- src/main/java/za/ac/cput/factory/UserDetailsFactory.java
+++ src/main/java/za/ac/cput/factory/UserDetailsFactory.java
@@ -1,4 +1,26 @@
+/*
+ * Address POJO Class
+ * Author: Haydn Abrahams
+ * Date: 04 April 2023
+ */
 package za.ac.cput.factory;
 
+import za.ac.cput.domain.UserDetails;
+import za.ac.cput.util.Helper;
+
 public class UserDetailsFactory {
+    public static UserDetails createUser(String userID, String dateOfReg, String loginDate, String password) {
+
+        if (Helper.isNullorEmpty(dateOfReg) || Helper.isNullorEmpty(loginDate)) {
+            return null;
+        }
+        userID = Helper.generateUserID();
+
+        return new UserDetails.Builder().setUserID(userID)
+                .setDateOfReg(dateOfReg)
+                .setLoginDate(loginDate)
+                .setPassword(password)
+                .build();
+    }
+
 }
