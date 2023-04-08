412f3622f3115b4b047b363803ab2d8604287be9
Cebisani0520 <cebisanilamani100@gmail.com>
2023-04-08 14:40:33 +0200

    Added Cart and the EmployeeAddress class

---
+++ src/main/java/za/ac/cput/domain/Cart.java
@@ -0,0 +1,102 @@
+/** Cart.java
+ * This is a POJO class
+ * This class is immutable (objects should not be changed)
+ * Cebisani Lamani (219104409)
+ * 17 March 2023
+ * */
+package za.ac.cput.domain;
+public class Cart {
+    private String cart_ID;
+    private Customer customer_ID;
+    private Product product_ID;
+    private int quantity;
+    private double total_Amount;
+
+    public Cart() {
+    }
+    // Builder Constructor
+    public Cart(Builder builder){
+        this.cart_ID = builder.cart_ID;
+        this.customer_ID = builder.customer_ID;
+        this.product_ID = builder.product_ID;
+        this.quantity = builder.quantity;
+        this.total_Amount = builder.total_Amount;
+    }
+
+
+    public String getCart_ID() {
+        return cart_ID;
+    }
+
+    public Customer getCustomer_ID() {
+        return customer_ID;
+    }
+
+    public Product getProduct_ID() {
+        return product_ID;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getTotal_Amount() {
+        return total_Amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Cart{" +
+                "booking_ID='" + cart_ID + '\'' +
+                ", customer_ID=" + customer_ID.getCustomer_ID() +
+                ", product_ID=" + product_ID +
+                ", quantity=" + quantity +
+                ", total_Amount=" + total_Amount +
+                '}';
+    }
+
+    public static class Builder{
+        private String cart_ID;
+        private Customer customer_ID;
+        private Product product_ID;
+        private int quantity;
+        private double total_Amount;
+
+        public Builder setCart_ID(String cart_ID) {
+            this.cart_ID = cart_ID;
+            return this;
+        }
+
+        public Builder setCustomer_ID(Customer customer_ID) {
+            this.customer_ID = customer_ID;
+            return this;
+        }
+
+        public Builder setProduct_ID(Product product_ID) {
+            this.product_ID = product_ID;
+            return this;
+        }
+
+        public Builder setQuantity(int quantity) {
+            this.quantity = quantity;
+            this.total_Amount = (quantity >= 1) ? product_ID.getProduct_Price() * quantity : 0.0;
+            return this;
+        }
+
+        public Builder setTotal_Amount(double total_Amount) {
+            this.total_Amount = total_Amount;
+            return this;
+        }
+
+        public Builder copy(Cart cart){
+            this.cart_ID = cart.cart_ID;
+            this.customer_ID = cart.customer_ID;
+            this.product_ID = cart.product_ID;
+            this.quantity = cart.quantity;
+            this.total_Amount = cart.total_Amount;
+            return this;
+        }
+
+        public Cart build(){return new Cart(this);}
+    }
+}
---
+++ src/main/java/za/ac/cput/domain/EmployeeAddress.java
@@ -0,0 +1,117 @@
+/** Cart.java
+ * This is a POJO class
+ * This class is immutable (objects should not be changed)
+ * Cebisani Lamani (219104409)
+ * 17 March 2023
+ * */
+package za.ac.cput.domain;
+
+public class EmployeeAddress {
+    private String address_ID;
+    private Employee employee_ID;
+    private String house_No;
+    private String street_Name;
+    private String suburb;
+    private String postal_Code;
+
+
+    public EmployeeAddress() {
+    }
+    // Builder Constructor
+    public EmployeeAddress(Builder builder){
+        this.address_ID = builder.address_ID;
+        this.employee_ID = builder.employee_ID;
+        this.house_No = builder.house_No;
+        this.street_Name = builder.street_Name;
+        this.suburb = builder.suburb;
+        this.postal_Code = builder.postal_Code;
+    }
+
+    public String getAddress_ID() {
+        return address_ID;
+    }
+
+    public Employee getEmployee_ID() {
+        return employee_ID;
+    }
+
+    public String getHouse_No() {
+        return house_No;
+    }
+
+    public String getStreet_Name() {
+        return street_Name;
+    }
+
+    public String getSuburb() {
+        return suburb;
+    }
+
+    public String getPostal_Code() {
+        return postal_Code;
+    }
+
+    @Override
+    public String toString() {
+        return "EmployeeAddress{" +
+                "address_ID=" + address_ID +
+                ", employee_ID=" + employee_ID.getEmployee_ID() +
+                ", house_No='" + house_No + '\'' +
+                ", street_Name='" + street_Name + '\'' +
+                ", suburb='" + suburb + '\'' +
+                ", postal_Code='" + postal_Code + '\'' +
+                '}';
+    }
+
+    public static class Builder{
+        private String address_ID;
+        private Employee employee_ID;
+        private String house_No;
+        private String street_Name;
+        private String suburb;
+        private String postal_Code;
+
+        public Builder setAddress_ID(String address_ID) {
+            this.address_ID = address_ID;
+            return this;
+        }
+
+        public Builder setEmployee(Employee employee) {
+            this.employee_ID = employee;
+            return this;
+        }
+
+        public Builder setHouse_No(String house_No) {
+            this.house_No = house_No;
+            return this;
+        }
+
+        public Builder setStreet_Name(String street_Name) {
+            this.street_Name = street_Name;
+            return this;
+        }
+
+        public Builder setSuburb(String suburb) {
+            this.suburb = suburb;
+            return this;
+        }
+
+        public Builder setPostal_Code(String postal_Code) {
+            this.postal_Code = postal_Code;
+            return this;
+        }
+        // Builder class copy builder
+        public Builder copy(EmployeeAddress employeeAddress){
+            this.address_ID = employeeAddress.address_ID;
+            this.employee_ID = employeeAddress.employee_ID;
+            this.house_No = employeeAddress.house_No;
+            this.street_Name = employeeAddress.street_Name;
+            this.suburb = employeeAddress.suburb;
+            this.postal_Code = employeeAddress.postal_Code;
+            return this;
+        }
+
+        public EmployeeAddress build(){return new EmployeeAddress(this);}
+
+    }
+}
