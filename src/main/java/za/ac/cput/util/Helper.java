3c98f462b258d58189b60b61e74233227800cd85
Cebisani0520 <cebisanilamani100@gmail.com>
2023-04-08 15:36:43 +0200

    Added a method.

--- src/main/java/za/ac/cput/util/Helper.java
+++ src/main/java/za/ac/cput/util/Helper.java
@@ -5,16 +5,20 @@
  */
 package za.ac.cput.util;
 
+/*import org.apache.commons.validator.routines.EmailValidator;*/
+
 import java.util.UUID;
 
 public class Helper {
 
     public static boolean isNullorEmpty(String i) {
         return (i == null || i.isEmpty() || i == " ");
-
     }
-
     public static String generateUserID() {
         return UUID.randomUUID().toString();
     }
+    public static String generateID(){
+        return UUID.randomUUID().toString();
+    }
+
 }
