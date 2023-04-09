f0ec8ffabcf56cf0ebdb131d7b3aa7fa21afb303
Cebisani0520 <cebisanilamani100@gmail.com>
2023-04-09 13:14:32 +0200

    Added CartFactoryTest.

--- src/test/java/za/ac/cput/factory/CartFactoryTest.java
+++ src/test/java/za/ac/cput/factory/CartFactoryTest.java
@@ -7,17 +7,16 @@ package za.ac.cput.factory;
 
 import org.junit.jupiter.api.Test;
 import za.ac.cput.domain.Cart;
-import za.ac.cput.domain.Customer;
-import za.ac.cput.domain.Product;
 import za.ac.cput.domain.Products;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CartFactoryTest {
     @Test
     public void test(){
-        Customer customer = CustomerFactory.createCustomer("Cebisani", "Lamani");
-        Products product = ProductFactory.createProduct("500g Tyre polish", 179.99);
+        Customer customer = CustomerFactory.createCustomer("219104409", "Cebisani", "Lamani");
+        Products product = ProductsFactory.createProductsID("500g Tyre polish", 179.99);
         Cart cart = CartFactory.addCart(customer, product, 3);
         assertNotNull(cart);
         System.out.println("Cart:" + cart);
