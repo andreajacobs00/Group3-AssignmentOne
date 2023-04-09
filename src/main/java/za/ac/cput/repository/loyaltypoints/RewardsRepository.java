b60733ec810853ad575a2034df3306dd255a1589
Andrea Jacobs <jacobsandrea02@gmail.com>
2023-04-09 12:57:35 +0200

    The repository interfaces were created.

---
+++ src/main/java/za/ac/cput/repository/loyaltypoints/RewardsRepository.java
@@ -0,0 +1,9 @@
+/** RewardsRepository.java
+ *  This is a repository class
+ *  Andrea Jacobs 218024266
+ *  09 April 2023 */
+
+package za.ac.cput.repository.loyaltypoints;
+
+public class RewardsRepository {
+}
---
+++ src/main/java/za/ac/cput/repository/packagesoffered/IRepository.java
@@ -0,0 +1,13 @@
+/** IRepository.java
+ *  This is the repository interface
+ *  Andrea Jacobs 218024266
+ *  09 April 2023 */
+
+package za.ac.cput.repository.packagesoffered;
+
+public interface IRepository <T, ID> {
+    T create(T t);
+    T read(ID id);
+    T update(T t);
+    boolean delete(ID id);
+}
