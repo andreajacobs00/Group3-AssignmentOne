bbbf57fab5e1a4507b1e69d79af4c8bffc561ef6
Haydn-Abrahams <100468876+Haydn-Abrahams@users.noreply.github.com>
2023-04-09 13:38:45 +0200

    Update IEmployeeRepo.java

--- src/main/java/za/ac/cput/repository/IEmployeeRepo.java
+++ src/main/java/za/ac/cput/repository/IEmployeeRepo.java
@@ -7,7 +7,7 @@ package za.ac.cput.repository;
 import za.ac.cput.domain.Employee;
 import java.util.Set;
 
-public interface IEmployeeRepo extends IRepo<Employee,String>{
+public interface IEmployeeRepo extends IRepository<Employee,String>{
     public Set<Employee> getAll();
 
 }
