121c639113388d015399d39a2c16103b8e61330a
Ziyaad <ziyaadh63@gmail.com>
2023-04-09 12:47:52 +0200

    This commit adds the BookingFactoryTest and the ProductsFactoryTest - Ziyaad Hendricks

---
+++ src/test/java/za/ac/cput/factory/BookingFactoryTest.java
@@ -0,0 +1,19 @@
+package za.ac.cput.factory;
+
+import org.junit.jupiter.api.Test;
+import za.ac.cput.domain.Booking;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookingFactoryTest {
+//    @Test
+//    public void createBooking(){
+//        Booking cBooking = BookingFactory.createBookingID("124415",
+//                "Sedan",
+//                "021 523 2354",
+//                "328",
+//                false);
+//        System.out.println(cBooking);
+//        assertNotNull(cBooking);
+//    }
+}
